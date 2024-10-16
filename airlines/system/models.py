dfa65d6e6050c3b5a47ab53194de5a2771f07e34
PlexMAD <113678445+PlexMAD@users.noreply.github.com>
2024-10-17 02:22:10 +0300

    создание таблиц

--- airlines/system/models.py
+++ airlines/system/models.py
@@ -92,3 +92,52 @@ class UserSessionTracking(models.Model):
         if self.logout_time:
             self.duration = self.logout_time - self.login_time
         super(UserSessionTracking, self).save(*args, **kwargs)
+
+
+class Airports(models.Model):
+    id = models.AutoField(db_column='ID', primary_key=True, blank=True)
+    countryid = models.ForeignKey(Countries, models.DO_NOTHING, db_column='CountryID')
+    iata_code = models.CharField(db_column='IATACode', max_length=3)
+    name = models.TextField(db_column='Name')
+
+    class Meta:
+        db_table = 'airports'
+
+
+class Routes(models.Model):
+    id = models.AutoField(db_column='ID', primary_key=True, blank=True)
+    departure_airport = models.ForeignKey(Airports, models.DO_NOTHING, db_column='DepartureAirportID',
+                                          related_name='departure_routes')
+    arrival_airport = models.ForeignKey(Airports, models.DO_NOTHING, db_column='ArrivalAirportID',
+                                        related_name='arrival_routes')
+    distance = models.IntegerField(db_column='Distance')
+    flight_time = models.TimeField(db_column='FlightTime')
+
+    class Meta:
+        db_table = 'routes'
+
+
+class Aircrafts(models.Model):
+    id = models.AutoField(db_column='ID', primary_key=True, blank=True)
+    name = models.TextField(db_column='Name')
+    make_model = models.TextField(db_column='MakeModel')
+    total_seats = models.IntegerField(db_column='TotalSeats')
+    economy_seats = models.IntegerField(db_column='EconomySeats')
+    business_seats = models.IntegerField(db_column='BusinessSeats')
+
+    class Meta:
+        db_table = 'aircrafts'
+
+
+class Schedules(models.Model):
+    id = models.AutoField(db_column='ID', primary_key=True, blank=True)
+    date = models.DateField(db_column='Date')
+    time = models.TimeField(db_column='Time')
+    aircraft = models.ForeignKey(Aircrafts, models.DO_NOTHING, db_column='AircraftID')
+    route = models.ForeignKey(Routes, models.DO_NOTHING, db_column='RouteID')
+    flight_number = models.CharField(db_column='FlightNumber', max_length=10)
+    economy_price = models.DecimalField(db_column='EconomyPrice', max_digits=10, decimal_places=2)
+    confirmed = models.BooleanField(db_column='Confirmed')
+
+    class Meta:
+        db_table = 'schedules'
